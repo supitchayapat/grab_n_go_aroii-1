ff63a1205103d4b9b10a260d155f96cd79df42b8
earthzzz <59070131@kmitl.ac.th>
2019-01-26 23:32:32 +0700

    add home page & images folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,8 +10,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 //    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter !',
-      home: RandomWords(),
+      title: 'Aroii',
+      theme: new ThemeData(
+        primaryColor: Colors.yellow,
+      ),
+      home: SearchMerchant(),
     );
   }
 }
@@ -103,15 +108,15 @@ class RandomWordsState extends State<RandomWords> {
     final List<WordPair> _suggestions = <WordPair>[];
     final Set<WordPair> _saved = new Set<WordPair>();
     final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
     Widget _buildSuggestion(){
       return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder:  (context, i){
+        padding: const EdgeInsets.all(30.0),
+        itemBuilder: (context, i){
           if (i.isOdd) return Divider();
-
           final index = i ~/ 2;
           if (index >= _suggestions.length){
-            _suggestions.addAll(generateWordPairs().take(10));
+            _suggestions.addAll(generateWordPairs().take(1));
           }
           return _buildRow(_suggestions[index]);
         });
@@ -141,9 +146,40 @@ class RandomWordsState extends State<RandomWords> {
     Widget build(BuildContext context) {
       return Scaffold(
         appBar:  AppBar(
-          title: Text('Startup Name Generator'),
+          title: Text('Grab & Go'),
+          actions: <Widget>[
+            new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+          ],
         ),
         body: _buildSuggestion(),
+      );
+    }
+    void _pushSaved() {
+      Navigator.of(context).push(
+        new MaterialPageRoute<void>(
+          builder: (BuildContext context){
+            final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair) {
+                return new ListTile(
+                  title: new Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+            );
+            final List<Widget> divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList();
+            return new Scaffold(
+              appBar: new AppBar(
+                title: const Text('Saved Suggestion'),
+              ),
+              body: new ListView(children: divided),
+            );
+          },
+        ),
       );
     }
 }
@@ -151,4 +187,49 @@ class RandomWordsState extends State<RandomWords> {
 class RandomWords extends StatefulWidget{
   @override
   RandomWordsState createState() => new RandomWordsState();
+}
+
+class Showmap{
+  Widget build(BuildContext context){
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/bts_map.jpg'),
+        )
+      )
+    );
+  }
+}
+
+class SearchMerchant extends StatefulWidget {
+  @override
+  SearchMerchantState createState() => new SearchMerchantState();
+}
+
+class SearchMerchantState extends State<SearchMerchant> {
+  int _selectedIndex = 0;
+  // final List<String> _MerchantList = ['A', 'B', 'C'];
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Grab & Go"),
+      ),
+      body: RandomWords(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(icon: Icon(Icons.restaurant), title: Text('Merchant')),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Profile'))
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.deepOrange,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+  void _onItemTapped(int index){
+    setState(() {
+          return 1;
+        });
+  }
 }
