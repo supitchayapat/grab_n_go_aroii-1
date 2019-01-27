a60c058a3c601a9f139e3dda8156f0db287761ef
dusitsiri <dusit.si@ku.th>
2019-01-27 13:47:01 +0700

    Merchant System Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,125 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => new _State();
-}
-
 class Order {
-  Order({this.name, this.orderNumber, this.price});
-  final String name;
-  final int orderNumber;
-  final int price;
+  String name;
+  int price;
+  int orderNumber;
+  bool confirm;
+
+  Order(this.name, this.price, this.orderNumber, this.confirm);
+  
 }
 
-class _State extends State<MyApp> {
+class OrderItemList extends StatefulWidget {
+  final Order order;
+
+  OrderItemList(Order order)
+      : order = order,
+        super(key: new ObjectKey(order));
+
+  @override
+  _OrderItemState createState() {
+    return new _OrderItemState(order);
+  }
+}
+
+class _OrderItemState extends State<OrderItemList> {
+  final Order order;
+
+  _OrderItemState(this.order);
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-        title: new Text('Confirm customer orders'),
+    return new ListTile(
+      leading: CircleAvatar(child: Text(order.orderNumber.toString())),
+      title: new Row(
+        children: <Widget>[
+          new Expanded(child: new Text(order.name)),
+          new Switch(
+              value: order.confirm,
+              onChanged: (bool value) {
+                setState(() {
+                  order.confirm = value;
+                });
+              })
+        ],
       ),
-      body: Container(
-        child: _buildContent(),
+      subtitle: Text('total: ' + order.price.toString() + ' baht'),
+    );
+  }
+}
+
+class OrderList extends StatefulWidget {
+  List<Order> order;
+
+  OrderList({Key key, this.order}) : super(key: key);
+
+  @override
+  _OrderListState createState() {
+    return new _OrderListState();
+  }
+}
+
+class _OrderListState extends State<OrderList> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Confirm Customer Orders'),
       ),
+      body: new Container(
+        padding: new EdgeInsets.all(8.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Expanded(
+                child: new ListView(
+              padding: new EdgeInsets.symmetric(vertical: 8.0),
+              children: widget.order.map((Order order) {
+                return new OrderItemList(order);
+              }).toList(),
+            )),
+            new RaisedButton(
+              onPressed: () {
+                for (Order o in widget.order) {
+                  if (o.confirm) print('Order '+o.orderNumber.toString()+ ': '+ o.name);
+                }
+              },
+              child: new Text('Save'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Demo App',
+      home: new OrderList(order: [
+        new Order('Isa Tusa', 150, 1, false),
+        new Order('Racquel Ricciardi', 50, 2, false),
+        new Order('Teresita Mccubbin', 75, 3, false),
+        new Order('Rhoda Hassinger', 65, 4, false),
+        new Order('Carson Cupps', 300, 5, false),
+        new Order('Devora Nantz', 100, 6, false),
+        new Order('Tyisha Primus', 40, 7, false),
+        new Order('Muriel Lewellyn', 1000, 8, false),
+        new Order('Hunter Giraud', 600, 9, false),
+        new Order('Corina Whiddon', 80, 10, false),
+        new Order('Meaghan Covarrubias', 90, 11, false),
+        new Order('Ulysses Severson', 120, 12, false),
+        new Order('Richard Baxter', 140, 13, false),
+        new Order('Alessandra Kahn', 110, 14, false),
+        new Order('Libby Saari', 200, 15, false),
+        new Order('Valeria Salvador', 220, 16, false),
+        new Order('Fredrick Folkerts', 350, 17, false),
+        new Order('Delmy Izzi', 450, 18, false),
+        new Order('Leann Klock', 700, 19, false),
+        new Order('Rhiannon Macfarlane', 750, 20, false),
+      ],),
     ));
-  }
-
-  Widget _buildContent() {
-    return ListView.builder(
-        itemCount: allOrders.length,
-        itemBuilder: (BuildContext content, int index) {
-          Order order = allOrders[index];
-          return OrderListTitle(order);
-        });
-  }
 }
-
-class OrderListTitle extends ListTile {
-  OrderListTitle(Order order)
-      : super(
-          title: Text('Order: ' + order.name),
-          subtitle: Text('total: ' + order.price.toString()),
-          leading: CircleAvatar(child: Text(order.orderNumber.toString())),
-        );
-}
-
-  List<Order> allOrders = [
-  Order(name: 'Isa Tusa', price: 150, orderNumber: 1),
-  Order(name: 'Racquel Ricciardi', price: 50, orderNumber: 2),
-  Order(name: 'Teresita Mccubbin', price: 75, orderNumber: 3),
-  Order(name: 'Rhoda Hassinger', price: 65, orderNumber: 4),
-  Order(name: 'Carson Cupps', price: 300, orderNumber: 5),
-  Order(name: 'Devora Nantz', price: 100, orderNumber: 6),
-  Order(name: 'Tyisha Primus', price: 40, orderNumber: 7),
-  Order(name: 'Muriel Lewellyn', price: 1000, orderNumber: 8),
-  Order(name: 'Hunter Giraud', price: 600, orderNumber: 9),
-  Order(name: 'Corina Whiddon', price: 80, orderNumber: 10),
-  Order(name: 'Meaghan Covarrubias', price: 90, orderNumber: 11),
-  Order(name: 'Ulysses Severson', price: 120, orderNumber: 12),
-  Order(name: 'Richard Baxter', price: 140, orderNumber: 13),
-  Order(name: 'Alessandra Kahn', price: 110, orderNumber: 14),
-  Order(name: 'Libby Saari', price: 200, orderNumber: 15),
-  Order(name: 'Valeria Salvador', price: 220, orderNumber: 16),
-  Order(name: 'Fredrick Folkerts', price: 350, orderNumber: 17),
-  Order(name: 'Delmy Izzi', price: 450, orderNumber: 18),
-  Order(name: 'Leann Klock', price: 700, orderNumber: 19),
-  Order(name: 'Rhiannon Macfarlane', price: 750, orderNumber: 20),
-];
-
-
-
 
 
